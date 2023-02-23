d5fd953aa668406fb1a87f6f4555bfc077e16b7d
Jialue Cao <phatcao@utexas.edu>
2023-02-23 13:20:18 -0600

    done with Collatz

--- lab1/test.java
+++ lab1/test.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class test {
+public class test {
+    public static int[] windowPosSum(int[] a, int n){
+        int lent=a.length;
+        for (int i=0;i<lent;i+=1){
+            int summ =0;
+            for (int k=i;k<=i+n;k+=1){
+                if (k<lent ){
+                        summ += a[k];
+                    }
+                else{
+                    break;
+                }
+            }
+            if (a[i]>0){
+            a[i]=summ;
+            }
+            else{
+                continue;
+            }
+        }
+        return a;
+    }
+    public static void main(String[] args){
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a,n);
+        System.out.println(java.util.Arrays.toString(a));
+
+
+
+    }
+
+
+
+
+
 }
